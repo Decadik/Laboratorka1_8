860d036f86861ccab4bf1e89efd59f44f5157d59
Decady <36299716+Decadik@users.noreply.github.com>
2025-10-06 11:14:33 +0300

    update result
    add diff

--- codefile.py
+++ codefile.py
@@ -1,4 +1,4 @@
 print("The Simple Code File")
 num_one = int(input("Введите число 1: "))
 num_two = int(input("Введите число 2: "))
-result_sum = num_one + num_two
+result_dif = abs(num_one - num_two)
